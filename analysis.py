12038ef72a085a5c0b133157a5f5109d44a37a25
FattiMei <teomei68@gmail.com>
2024-04-08 21:16:45 +0200

    analysis.py can handle multiple benchmarks in a single file

--- analysis.py
+++ analysis.py
@@ -2,19 +2,17 @@ import numpy as np
 import sys
 
 
-def parse_file(filename):
-    with open(filename, 'r') as f:
-        title = f.readline()
-        data  = np.array([float(line) for line in f.readlines()])
+def parse_chunk(chunk):
+    lines = chunk.split('\n')
 
-    return (title, data)
+    title = lines[0]
+    data  = [float(entry) for entry in lines[1:] if entry]
 
-
-def compute_statistics(data):
     return {
-        'mean'    : np.mean(data),
-        'median'  : np.median(data),
-        'min'     : np.min(data),
+        'title' : title,
+        'mean'  : np.mean(data),
+        'median': np.median(data),
+        'min'   : np.min(data)
     }
 
 
@@ -23,10 +21,9 @@ if __name__ == '__main__':
         print('No files were given to analyze... quitting')
         sys.exit(1)
 
+    for filename in sys.argv[1:]:
+        with open(filename, 'r') as f:
+            tests = [parse_chunk(x) for x in f.read().split('# ') if x]
 
-    for bench_report_filename in sys.argv[1:]:
-        title, data = parse_file(bench_report_filename)
-
-        print(title)
-        print(compute_statistics(data))
-        print()
+            print(f"Test filename: {filename}")
+            print(tests)
